3a2757a3d92128b7038514754f26decae4ad876f
Danielius Visockas <danieliusvisockas@gmail.com>
2021-06-08 10:21:52 +0300

    Use long uint for dequeue and change c function name

--- ext/vtslv/vtslv.cpp
+++ ext/vtslv/vtslv.cpp
@@ -7,7 +7,7 @@
 
 #include <rice/rice.hpp>
 
-double are_of_intersection(Rice::Object self, Rice::String wkt_poly1, Rice::String wkt_poly2) {
+double area_of_intersection(Rice::Object self, Rice::String wkt_poly1, Rice::String wkt_poly2) {
     typedef boost::geometry::model::polygon<boost::geometry::model::d2::point_xy<double> > polygon;
 
     std::string std_str_poly1 = wkt_poly1.str();
@@ -22,8 +22,8 @@ double are_of_intersection(Rice::Object self, Rice::String wkt_poly1, Rice::Stri
 
     boost::geometry::intersection(poly1, poly2, intersection_deque);
 
-    for (int i = 0; i < intersection_deque.size(); i++) {
-        intersection_area += boost::geometry::area(intersection_deque[i]);
+    for (unsigned long int i = 0; i < intersection_deque.size(); i++) {
+      intersection_area += boost::geometry::area(intersection_deque[i]);
     }
 
     return intersection_area;
@@ -35,5 +35,5 @@ void Init_vtslv()
 {
   Rice::Class rb_cTest =
     Rice::define_class("VTSLV")
-    .define_singleton_method("area_of_intersection", &are_of_intersection, Rice::Arg("wkt_poly1"), Rice::Arg("wkt_poly2"));
+    .define_singleton_method("area_of_intersection", &area_of_intersection, Rice::Arg("wkt_poly1"), Rice::Arg("wkt_poly2"));
 }
